7226362fb86e07f22518571d7eb549055168dbe3
Alper Ozaydin <aozaydi1@binghamton.edu>
2017-05-15 23:00:26 +0200

    inital commit

---
+++ GermanApp/FirstViewController.swift
@@ -0,0 +1,74 @@
+//
+//  FirstViewController.swift
+//  GermanApp
+//
+//  Created by Seric on 2/8/17.
+//  Copyright © 2017 Alper. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    var german = [String]()
+    var english = [String]()
+    
+    var randomIndex: Int = 0
+
+    @IBOutlet weak var TextView: UILabel!
+    
+    @IBOutlet weak var TextView2: UILabel!
+    
+    @IBAction func Answer(_ sender: Any) {
+        
+        TextView2.text = german[randomIndex]
+
+    }
+    
+    
+    @IBAction func Next(_ sender: Any) {
+        
+        randomIndex = Int(arc4random_uniform(UInt32(english.count)))
+        TextView.text = english[randomIndex]
+        TextView2.text = ""
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let path = Bundle.main.path(forResource: "essenundtrinken", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                let myStrings = data.components(separatedBy: .newlines)
+                var temp = [String]()
+                
+                for i in 0...(myStrings.count-2){
+                    
+                    temp = myStrings[i].components(separatedBy: ",")
+                    german.append(temp[0])
+                    english.append(temp[1])
+                }
+                
+                randomIndex = Int(arc4random_uniform(UInt32(english.count)))
+                TextView.text = english[randomIndex]
+                
+            } catch {
+                print(error)
+            }
+        }
+
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
